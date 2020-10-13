065a5d2e5d3454dc20e6a33857d0a983963ded61
Omar <omarimran2000@gmail.com>
2020-10-13 18:29:43 -0400

    Updated with constructors 2.0

--- src/Map.java
+++ src/Map.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Map {
+    String name;
+    ArrayList<Continent> continents;
+
+    public Map(String name, ArrayList<Continent> continents) {
+        this.name = name;
+        this.continents = continents;
+    }
 }
