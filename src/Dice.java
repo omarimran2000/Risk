81c854cc0b6c10f563801c2c163f4b633a7cf3e0
santhosh <santhoshpradeepan@cmail.carleton.ca>
2020-10-13 17:51:51 -0400

    updated Dice class

--- src/Dice.java
+++ src/Dice.java
@@ -7,6 +7,24 @@ public class Dice {
         random = new Random();
     }
     private int roll() {
+        return random.nextInt(6) + 1;
+    }
 
+    public int[] rollDice(int n) {
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = roll();
+        }
+        return sortDice(answer, n);
+    }
+
+    private int[] sortDice(int[] dice, int size) {
+        if (size == 0) {
+
+        }
+        else if (size > 1) {
+
+        }
+        return dice;
     }
 }
