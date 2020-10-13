d538b302bd51af674a4586eada97e909ecf8b580
santhosh <santhoshpradeepan@cmail.carleton.ca>
2020-10-13 18:02:36 -0400

    finished basic dice class

--- src/Dice.java
+++ src/Dice.java
@@ -15,16 +15,25 @@ public class Dice {
         for (int i = 0; i < n; i++) {
             answer[i] = roll();
         }
-        return sortDice(answer, n);
+        return sortDice(answer);
     }
 
-    private int[] sortDice(int[] dice, int size) {
+    private int[] sortDice(int[] results) {
+        int size = results.length;
         if (size == 0) {
-
+            // either throw an error if this is an error or just ignore, not sure.
         }
         else if (size > 1) {
-
+            for (int i = 2; i < size; i++) {
+                int temp = results[i];
+                int j = i - 1;
+                while (j > 0 && results[j] > temp) {
+                    results[j + 1] = results[j];
+                    j--;
+                }
+                results[j + 1] = temp;
+            }
         }
-        return dice;
+        return results;
     }
 }
