35f20018d0b2152e4aa918eaaded02cc9ffc778e
Omar <omarimran2000@gmail.com>
2020-10-13 20:15:20 -0400

    Added Javadocs

--- src/Continent.java
+++ src/Continent.java
@@ -1,3 +1,10 @@
+/**
+ * The continent class that represents a continent on the board
+ *
+ * @author Omar Imran
+ * @date October 13 2020
+ */
+
 import java.util.ArrayList;
 
 public class Continent {
@@ -6,29 +13,54 @@ public class Continent {
     private ArrayList<Territory> territories;
     private int continentPoint;
 
+    /**
+     * Default constructor
+     *
+     * @param name the name of the continent
+     * @param territories the territories located in the continent
+     * @param continentPoint the amount of extra soldiers a player gets for holding the continent
+     */
     public Continent(String name, ArrayList<Territory> territories, int continentPoint) {
         this.name = name;
         this.territories = territories;
         this.continentPoint = continentPoint;
     }
+
+    /**
+     * Getter function for name
+     *
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter function for territories
+     * @return territories
+     */
     public ArrayList<Territory> getTerritories() {
         return territories;
     }
 
+    /**
+     * Getter function for extra points
+     * @return the amount of extra soldiers
+     */
     public int getContinentPoint() {
         return continentPoint;
     }
-    public boolean getControl(){
 
-        Player firstPlayer = territories.get(0).getCurrentPlayer();
+    /**
+     * Checks to see if a player has all the territories in the continent
+     * @param p the player being compared
+     * @return if player holds entire continent or not
+     */
+    public boolean getControl(Player p){
 
         for (Territory territory:territories)
         {
-            if (!(territory.getCurrentPlayer().equals(firstPlayer)))
+            if (!(territory.getCurrentPlayer().equals(p)))
             {
                 return false;
             }
